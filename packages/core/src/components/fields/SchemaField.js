9723a73b945ace6fcefdbfed0b199c5d8a059ec8
Sean <dokko1230@gmail.com>
2020-07-23 13:09:13 -0700

    Support for Circular Referencing in RJSF (#14)
    * fix(circular-ref): prevent SchemaField from rendering when there is circular referencing
    
    * refactor(circular-referencing): handle more scenarios of circular referencing
    
    * test: remove scenario covering a circular reference in Form_test.js
    
    * test: remove recursive support unit test (it has a circular reference)
    
    * test: remove irrelevant test
    
    * feat(circular-ref): support for anyOf, allOf, oneOf in the cyclical detector
    
    * test(circular-ref): disable support for the anyOf infinite circular scenario
    
    * Update packages/core/test/utils_test.js
    
    Co-authored-by: Jon Ursenbach <erunion@users.noreply.github.com>
    
    * fix: typo
    
    Co-authored-by: Jon Ursenbach <erunion@users.noreply.github.com>

--- packages/core/src/components/fields/SchemaField.js
+++ packages/core/src/components/fields/SchemaField.js
@@ -7,6 +7,7 @@ import * as types from "../../types";
 import {
   isMultiSelect,
   isSelect,
+  isCyclic,
   retrieveSchema,
   toIdSchema,
   getDefaultRegistry,
@@ -240,14 +241,20 @@ function SchemaFieldRender(props) {
     wasPropertyKeyModified = false,
   } = props;
   const { rootSchema, fields, formContext } = registry;
+  if (isCyclic(props.schema, rootSchema)) {
+    return null;
+  }
+
   const FieldTemplate =
     uiSchema["ui:FieldTemplate"] || registry.FieldTemplate || DefaultTemplate;
   let idSchema = props.idSchema;
   const schema = retrieveSchema(props.schema, rootSchema, formData);
+
   idSchema = mergeObjects(
     toIdSchema(schema, null, rootSchema, formData, idPrefix),
     idSchema
   );
+
   const FieldComponent = getFieldComponent(
     schema,
     uiSchema,
