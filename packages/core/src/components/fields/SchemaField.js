4d38593a42522dbdab879cf863aa09781bb09769
Sean Dokko <dokko1230@gmail.com>
2020-09-17 11:10:35 -0700

    fix: do not check isCyclic at SchemaField. It is not needed since SchemaField will not render nested array schemas (a user driven addition action)

--- packages/core/src/components/fields/SchemaField.js
+++ packages/core/src/components/fields/SchemaField.js
@@ -7,7 +7,6 @@ import * as types from "../../types";
 import {
   isMultiSelect,
   isSelect,
-  isCyclic,
   retrieveSchema,
   toIdSchema,
   getDefaultRegistry,
@@ -241,9 +240,6 @@ function SchemaFieldRender(props) {
     wasPropertyKeyModified = false,
   } = props;
   const { rootSchema, fields, formContext } = registry;
-  if (isCyclic(props.schema, rootSchema)) {
-    return null;
-  }
 
   const FieldTemplate =
     uiSchema["ui:FieldTemplate"] || registry.FieldTemplate || DefaultTemplate;
